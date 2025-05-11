3d67a3edbcfbc84caef1f03ebbfd09604616a52e
cgoolsby <cvioling@gmail.com>
2025-05-11 17:23:31 -0500

    refactor: replace kubernetes_namespace with null_resource for flux namespace creation

--- infra/cluster-iac/namespace.tf
+++ infra/cluster-iac/namespace.tf
@@ -1,10 +1,8 @@
 resource "null_resource" "create_flux_ns" {
   provisioner "local-exec" {
     command = <<-EOT
-      aws eks update-kubeconfig \
-        --region ${var.region} \
-        --name ${module.eks.cluster_name}
-
+      export KUBECONFIG="$(mktemp)"
+      aws eks update-kubeconfig --region ${var.region} --name ${module.eks.cluster_name} --kubeconfig "$KUBECONFIG"
       kubectl get ns flux-system || kubectl create ns flux-system
     EOT
     interpreter = ["/bin/bash", "-c"]
